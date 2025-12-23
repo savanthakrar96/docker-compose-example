2d236600b58409bf48e911299bb408ffec0b3fac
savanthakrar96 <savanthakarar41@gmail.com>
2025-12-23 13:38:53 +0530

    create the table

--- server.js
+++ server.js
@@ -7,19 +7,39 @@ app.use(express.json())
 
 
 //routes
-app.get('/', (req,res) => {
-    res.sendStatus(200)
+app.get('/', async (req,res) => {
+    try {
+        const data = await pool.query('SELECT * FROM schools')
+        res.status(200).send(data.rows)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
 })
 
-app.post('/', (req,res) => {
+app.post('/', async (req,res) => {
     const { name, location } = req.body
-    res.status(200).send({
-        message: `Your Keys Were ${name}, ${location}`
-    })
+   try {
+        await pool.query('INSERT INTO schools (name,address) VALUES ($1,$2)', [name, location])
+        res.status(200).send({
+            message: "Successfully added child"
+        })     
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
 })
 
-app.get('setup', (req,res) => {
-    
+app.get('/setup',async (req,res) => {
+    try {
+        await pool.query('CREATE TABLE schools(id  SERIEAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100) )') 
+        res.status(200).send({
+            message: "Successfully created table"
+        })    
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
 })
 
 app.listen(port, () => console.log(`Server has started on : ${port}`))
